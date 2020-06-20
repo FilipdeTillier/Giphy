b8ce0186acc60962764f345aa67dfb336456d333
unknown <filip850@wp.pl>
2020-06-21 00:36:51 +0200

    change catch

--- server/services/giphyService.ts
+++ server/services/giphyService.ts
@@ -1,22 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
-import { giphyMapData } from 'helpers';
+import { giphyMapData } from '../helpers/index';
 
 const GIPHY_API_KEY: string = 'vhhkB8u81GKIKahrnq4Miw5ZjAlA9lhi';
 const GIPHY_API_URL: string = 'http://api.giphy.com/v1/gifs/search?';
 
 async function searchGiphy(q: string, limit: number, offset: string) {
   const url: string = `${GIPHY_API_URL}q=${q}&limit=${limit}&offset=${offset}`;
-  try {
-    const response: AxiosResponse = await axios.get(url, {
-      headers: {
-        api_key: GIPHY_API_KEY,
-      },
-    });
-    const data = giphyMapData(response.data.data);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+  const response: AxiosResponse = await axios.get(url, {
+    headers: {
+      api_key: GIPHY_API_KEY,
+    },
+  });
+  const data = giphyMapData(response.data.data);
+  return data;
 }
 
 export default searchGiphy;
--- server/services/pixBayService.ts
+++ server/services/pixBayService.ts
@@ -1,18 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
-import { pixBayMapData } from 'helpers';
+import { pixBayMapData } from '../helpers/index';
 
 const PIXBAY_API_KEY: string = '17139964-6ef728ffe6e2cd91bdda41ec2';
 const PIXBAY_API_URL: string = `https://pixabay.com/api/?key=${PIXBAY_API_KEY}`;
 
 async function serachPixBay(q: string, per_page: number, page: string) {
   const url: string = `${PIXBAY_API_URL}&q=${q}&per_page=${per_page}&page=${page}`;
-  try {
-    const response: AxiosResponse = await axios.get(url);
-    const pixBayData = pixBayMapData(response.data);
-    return pixBayData;
-  } catch (err) {
-    console.log(err);
-  }
+  const response: AxiosResponse = await axios.get(url);
+  const pixBayData = pixBayMapData(response.data);
+  return pixBayData;
 }
 
 export default serachPixBay;
