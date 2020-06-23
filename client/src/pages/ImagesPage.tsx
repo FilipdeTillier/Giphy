7173f59a9a5a421f6412a6d7eb9d61ec6a807e91
unknown <filip850@wp.pl>
2020-06-23 09:48:50 +0200

    load more button

--- client/src/pages/ImagesPage.tsx
+++ client/src/pages/ImagesPage.tsx
@@ -1,30 +1,60 @@
 import React, { useEffect, useState } from 'react';
 
 import ImagesSearch from '../components/ImagesSearch/ImagesSearch';
-import { getGiphy } from '../services/giphyService';
+import * as giphyService from '../services/giphyService';
 import ImagesList from '../components/ImagesList/ImagesList';
 
 const ImagesPage: React.FC = () => {
   const [giphys, setGiphys] = useState<string[]>([]);
   const [offset, setOffset] = useState<number>(1);
-  const perPage = 10;
+  const perPage = 20;
 
   function loadMore() {
     setOffset(offset + 1);
   }
 
+  async function getGiphy(
+    searchValue: string,
+    offset: number,
+    limit: number = perPage
+  ) {
+    try {
+      const giphy: string[] = await giphyService.getGiphy(
+        searchValue,
+        offset,
+        limit
+      );
+      return giphy;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   async function onSearchGiphy(searchValue: string) {
     try {
-      const giphy = await getGiphy(searchValue, offset, perPage);
-      setGiphys([...giphy]);
+      const giphy = await getGiphy(searchValue, 1);
+      giphy?.length && setGiphys(giphy);
     } catch (err) {
       console.log(err);
     }
   }
 
   useEffect(() => {
-    console.log(offset);
+    async function get() {
+      const giphy = await getGiphy('Burgers', offset);
+      giphy?.length && setGiphys([...giphys, ...giphy]);
+    }
+    if (offset > 1) {
+      get();
+    }
   }, [offset]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', (e) => console.log(e));
+    return () => {
+      document.removeEventListener('scroll', (e) => console.log(e));
+    };
+  });
 
   return (
     <div>
